5abdfbc9a1adb8b738cf20200bb2f3db2a2dcbc1
小莫 <xm@aoe.top>
2025-10-11 15:55:00 +0800

    添加 Google 分析

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,8 +1,7 @@
 import { withMermaid } from "vitepress-plugin-mermaid";
-import { generateSidebar } from 'vitepress-sidebar';
-import { imagetools } from 'vite-imagetools';
-import viteCompression from 'vite-plugin-compression';
-
+import { generateSidebar } from "vitepress-sidebar";
+import { imagetools } from "vite-imagetools";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitepress.dev/reference/site-config
 export default withMermaid({
@@ -10,7 +9,7 @@ export default withMermaid({
     description: "小莫的wiki库,记录一些有用的合集",
     ignoreDeadLinks: true,
     markdown: {
-        theme: 'material-theme-palenight',
+        theme: "material-theme-palenight",
         lineNumbers: true,
     },
     mermaid: {
@@ -20,29 +19,36 @@ export default withMermaid({
         plugins: [
             imagetools({
                 defaultDirectives: new URLSearchParams({
-                    format: 'webp',
-                    quality: '80',
-                    w: '800',
+                    format: "webp",
+                    quality: "80",
+                    w: "800",
                 }),
             }),
-            viteCompression()
+            viteCompression(),
         ],
     },
     themeConfig: {
-        logo: 'https://mod.3dmgame.com/static/upload/mod/202307/MOD64a7767d0409f.png@webp',
+        logo: "https://mod.3dmgame.com/static/upload/mod/202307/MOD64a7767d0409f.png@webp",
         // https://vitepress.dev/reference/default-theme-config
         nav: [
-            { text: '首页', link: '/' },
-            { text: 'GMM', link: 'https://gmm.aoe.top/' },
+            { text: "首页", link: "/" },
+            { text: "GMM", link: "https://gmm.aoe.top/" },
             {
-                text: "小莫全家桶", items: [
-                    { text: '首页', link: 'https://www.aoe.top/' },
-                    { text: '博客', link: 'https://blog.aoe.top/' },
-                    { text: '网盘', link: 'https://pan.aoe.top/' },
-                    { text: '游戏', link: 'https://game.aoe.top/' },
-                    { text: 'Mod', link: 'https://mod.3dmgame.com/u/9688990/Home' },
-                    { text: '买下我', link: 'https://wanwang.aliyun.com/nametrade/detail/online.html?domainName=aoe.top' },
-                ]
+                text: "小莫全家桶",
+                items: [
+                    { text: "首页", link: "https://www.aoe.top/" },
+                    { text: "博客", link: "https://blog.aoe.top/" },
+                    { text: "网盘", link: "https://pan.aoe.top/" },
+                    { text: "游戏", link: "https://game.aoe.top/" },
+                    {
+                        text: "Mod",
+                        link: "https://mod.3dmgame.com/u/9688990/Home",
+                    },
+                    {
+                        text: "买下我",
+                        link: "https://wanwang.aliyun.com/nametrade/detail/online.html?domainName=aoe.top",
+                    },
+                ],
             },
         ],
 
@@ -50,45 +56,46 @@ export default withMermaid({
         sidebar: generateSidebar({
             useTitleFromFileHeading: true,
             collapsed: true,
-            documentRootPath: '/docs',
-            sortFolderTo: 'bottom',
+            documentRootPath: "/docs",
+            sortFolderTo: "bottom",
             useFolderTitleFromIndexFile: true,
             includeFolderIndexFile: false,
         }),
 
         socialLinks: [
-            { icon: 'github', link: 'https://github.com/3DMXM/wiki.aoe.top' },
+            { icon: "github", link: "https://github.com/3DMXM/wiki.aoe.top" },
         ],
         search: {
-            provider: 'local'
+            provider: "local",
         },
         editLink: {
             text: "在GitHub上编辑此页面",
-            pattern: 'https://github.com/3DMXM/wiki.aoe.top/edit/master/docs/:path'
+            pattern:
+                "https://github.com/3DMXM/wiki.aoe.top/edit/master/docs/:path",
         },
         lastUpdated: {
-            text: '最后更新于',
+            text: "最后更新于",
             formatOptions: {
-                dateStyle: 'short',
-                timeStyle: 'medium'
-            }
+                dateStyle: "short",
+                timeStyle: "medium",
+            },
         },
         footer: {
-            message: '基于 MIT 许可发布',
+            message: "基于 MIT 许可发布",
             copyright: `版权所有 © 2022-${new Date().getFullYear()} 小莫 <br/> 
             <a href="https://beian.miit.gov.cn/" target="_blank" rel="nopener norefoerrer" data-v-a41d4df4="">湘ICP备2021011453号-1</a>`,
         },
         docFooter: {
-            prev: '上一页',
-            next: '下一页'
+            prev: "上一页",
+            next: "下一页",
         },
         outline: {
-            label: '页面导航'
+            label: "页面导航",
         },
     },
     head: [
         [
-            'script',
+            "script",
             {},
             `var _hmt = _hmt || [];
             (function() {
@@ -96,22 +103,29 @@ export default withMermaid({
               hm.src = "https://hm.baidu.com/hm.js?0fba1f3013c5565107a4b39759647150";
               var s = document.getElementsByTagName("script")[0]; 
               s.parentNode.insertBefore(hm, s);
-            })();`
+            })();`,
         ],
-        ['meta', { name: 'og:site_name', content: '小莫的维基库' }],
-        ['meta', { name: 'og:locale', content: 'zh' }],
-        ['meta', { name: 'og:type', content: 'website' }],
-        ['meta', { name: 'theme-color', content: '#5f67ee' }],
         [
-            'link',
+            "script",
             {
-                "rel": "icon",
-                "href": "https://mod.3dmgame.com/static/upload/mod/202307/MOD64a7767d0409f.png@webp",
-            }
-        ]
+                async: "true",
+                src: "https://www.googletagmanager.com/gtag/js?id=G-L04H04RSS7",
+            },
+        ],
+        ["meta", { name: "og:site_name", content: "小莫的维基库" }],
+        ["meta", { name: "og:locale", content: "zh" }],
+        ["meta", { name: "og:type", content: "website" }],
+        ["meta", { name: "theme-color", content: "#5f67ee" }],
+        [
+            "link",
+            {
+                rel: "icon",
+                href: "https://mod.3dmgame.com/static/upload/mod/202307/MOD64a7767d0409f.png@webp",
+            },
+        ],
     ],
     sitemap: {
-        hostname: "https://wiki.aoe.top"
+        hostname: "https://wiki.aoe.top",
     },
     lastUpdated: true,
-})
+});
